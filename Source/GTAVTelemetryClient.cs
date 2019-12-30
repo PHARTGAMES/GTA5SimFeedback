36977e7a61b00c1754bf7c7a8e063b814229a46e
PHARTGAMES <phartgames@gmail.com>
2019-12-30 17:08:19 +1000

    GTAVSimFeedback release v1.0

---
+++ Source/GTAVTelemetryClient.cs
@@ -0,0 +1,150 @@
+﻿//
+// Copyright (c) 2019 Rausch IT
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+// copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in 
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+// THE SOFTWARE.
+//
+//
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.MemoryMappedFiles;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Windows.Forms;
+using GTA;
+using GTA.Native;
+using GTA.Math;
+
+namespace GTAVTelemetry
+{
+    class GTA5TelemetryClient : Script
+    {
+        GTAVAPI dataPacket = new GTAVAPI(); 
+        private Vector3 lastLocalVelocity = new Vector3(0, 0, 0);
+        private Vector3 lastAngularVelocity = new Vector3(0, 0, 0);
+        private TelemetrySender telemetrySender;
+        private Int32 port = 20777;
+
+        public GTA5TelemetryClient()
+        {
+            dataPacket.Initialize();
+            telemetrySender = new TelemetrySender(port);
+            Tick += OnTick;
+            Interval = 0;// 10;
+        }
+
+
+        void OnTick(object sender, EventArgs e)
+        {
+            Ped player = Game.Player.Character;
+
+            if (player.IsInVehicle())
+            {
+                // Player in vehicle
+                Vehicle vehicle = player.CurrentVehicle;
+
+                Matrix ltw = vehicle.Matrix;
+
+                ltw.M41 = 0;
+                ltw.M42 = 0;
+                ltw.M43 = 0;
+
+                Vector3 localVelocity = ltw.InverseTransformPoint(vehicle.Velocity);
+
+                localVelocity.X = float.IsNaN(localVelocity.X) ? 0.0f : localVelocity.X;
+                localVelocity.Y = float.IsNaN(localVelocity.Y) ? 0.0f : localVelocity.Y;
+                localVelocity.Z = float.IsNaN(localVelocity.Z) ? 0.0f : localVelocity.Z;
+
+                Vector3 acceleration = localVelocity - lastLocalVelocity;
+
+                dataPacket.AccG[0] = float.IsNaN(acceleration.X) ? 0.0f : acceleration.X;
+                dataPacket.AccG[1] = float.IsNaN(acceleration.Y) ? 0.0f : acceleration.Y;
+                dataPacket.AccG[2] = float.IsNaN(acceleration.Z) ? 0.0f : acceleration.Z;
+
+                lastLocalVelocity = localVelocity;
+
+                dataPacket.SpeedKmh = vehicle.Speed;
+                dataPacket.Rpm = vehicle.CurrentRPM;
+
+                dataPacket.LocalAngularVelocity[0] = float.IsNaN(vehicle.RotationVelocity.X) ? 0.0f : vehicle.RotationVelocity.X;
+                dataPacket.LocalAngularVelocity[1] = float.IsNaN(vehicle.RotationVelocity.Y) ? 0.0f : vehicle.RotationVelocity.Y;
+                dataPacket.LocalAngularVelocity[2] = float.IsNaN(vehicle.RotationVelocity.Z) ? 0.0f : vehicle.RotationVelocity.Z; 
+
+                dataPacket.LocalVelocity[0] = localVelocity.X;
+                dataPacket.LocalVelocity[1] = localVelocity.Y;
+                dataPacket.LocalVelocity[2] = localVelocity.Z;
+
+                Vector3 angularAcceleration = (vehicle.RotationVelocity - lastAngularVelocity) * (180.0f / 3.14f);
+
+                dataPacket.Pitch = float.IsNaN(vehicle.Rotation.X + angularAcceleration.X) ? 0.0f : vehicle.Rotation.X + angularAcceleration.X;
+                dataPacket.Roll = float.IsNaN(vehicle.Rotation.Y + angularAcceleration.Y) ? 0.0f : vehicle.Rotation.Y + angularAcceleration.Y;
+                dataPacket.Yaw = float.IsNaN(vehicle.Rotation.Z + angularAcceleration.Z) ? 0.0f : vehicle.Rotation.Z + angularAcceleration.Z;
+
+                lastAngularVelocity = vehicle.RotationVelocity;
+
+            }
+            else //walkies
+            {
+
+                Matrix ltw = player.Matrix;
+
+                ltw.M41 = 0;
+                ltw.M42 = 0;
+                ltw.M43 = 0;
+
+                Vector3 localVelocity = ltw.InverseTransformPoint(player.Velocity);
+
+                localVelocity.X = float.IsNaN(localVelocity.X) ? 0.0f : localVelocity.X;
+                localVelocity.Y = float.IsNaN(localVelocity.Y) ? 0.0f : localVelocity.Y;
+                localVelocity.Z = float.IsNaN(localVelocity.Z) ? 0.0f : localVelocity.Z;
+
+                Vector3 acceleration = localVelocity - lastLocalVelocity;
+
+                acceleration *= 0.125f; // need to scale this a bit otherwise it's super jerky
+
+                dataPacket.AccG[0] = float.IsNaN(acceleration.X) ? 0.0f : acceleration.X;
+                dataPacket.AccG[1] = float.IsNaN(acceleration.Y) ? 0.0f : acceleration.Y;
+                dataPacket.AccG[2] = float.IsNaN(acceleration.Z) ? 0.0f : acceleration.Z;
+
+                lastLocalVelocity = localVelocity;
+
+                dataPacket.LocalAngularVelocity[0] = 0.0f;
+                dataPacket.LocalAngularVelocity[1] = 0.0f;
+                dataPacket.LocalAngularVelocity[2] = 0.0f;
+
+                dataPacket.Pitch = float.IsNaN(player.Rotation.X) ? 0.0f : player.Rotation.X;
+                dataPacket.Roll = float.IsNaN(player.Rotation.Y) ? 0.0f : player.Rotation.Y;
+                dataPacket.Yaw = float.IsNaN(player.Rotation.Z) ? 0.0f : player.Rotation.Z;
+
+                lastAngularVelocity = Vector3.Zero;
+            }
+
+            //loop packet id
+            if (dataPacket.PacketId == int.MaxValue)
+                dataPacket.PacketId = 0;
+
+            dataPacket.PacketId++;
+
+            byte[] bytes = dataPacket.ToByteArray();
+            telemetrySender.SendAsync(bytes);
+        }
+    }
+}
---
+++ Source/GTAVTelemetryProvider.cs
@@ -0,0 +1,184 @@
+﻿
+//
+// Copyright (c) 2019 Rausch IT
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy 
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights 
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
+// copies of the Software, and to permit persons to whom the Software is 
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in 
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+// THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 
+// THE SOFTWARE.
+//
+//
+using SimFeedback.log;
+using SimFeedback.telemetry;
+using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace GTAVTelemetry
+{
+    /// <summary>
+    /// Assetto Corsa Telemetry Provider
+    /// </summary>
+    public sealed class GTAVTelemetryProvider : AbstractTelemetryProvider
+    {
+        private bool isStopped = true;                                  // flag to control the polling thread
+        private Thread t;                                               // the polling thread, reads telemetry data and sends TelemetryUpdated events
+        int PORTNUM = 20777;
+        private IPEndPoint _senderIP;                   // IP address of the sender for the udp connection used by the worker thread
+
+        /// <summary>
+        /// Default constructor.
+        /// Every TelemetryProvider needs a default constructor for dynamic loading.
+        /// Make sure to call the underlying abstract class in the constructor.
+        /// </summary>
+        public GTAVTelemetryProvider() : base()
+        {
+            Author = "PEZZALUCIFER";
+            Version = "v1.0";
+            BannerImage = @"img\banner_gtav.png"; // Image shown on top of the profiles tab
+            IconImage = @"img\gtav.jpg";          // Icon used in the tree view for the profile
+            TelemetryUpdateFrequency = 100;     // the update frequency in samples per second
+        }
+
+        /// <summary>
+        /// Name of this TelemetryProvider.
+        /// Used for dynamic loading and linking to the profile configuration.
+        /// </summary>
+        public override string Name { get { return "gtav"; } }
+
+        public override void Init(ILogger logger)
+        {
+            base.Init(logger);
+            Log("Initializing GTAVTelemetryProvider");
+        }
+
+        /// <summary>
+        /// A list of all telemetry names of this provider.
+        /// </summary>
+        /// <returns>List of all telemetry names</returns>
+        public override string[] GetValueList()
+        {
+            return GetValueListByReflection(typeof(GTAVAPI));
+        }
+
+        /// <summary>
+        /// Start the polling thread
+        /// </summary>
+        public override void Start()
+        {
+            if (isStopped)
+            {
+                LogDebug("Starting GTAVTelemetryProvider");
+                isStopped = false;
+                t = new Thread(Run);
+                t.Start();
+            }
+        }
+
+        /// <summary>
+        /// Stop the polling thread
+        /// </summary>
+        public override void Stop()
+        {
+            LogDebug("Stopping GTAVTelemetryProvider");
+            isStopped = true;
+            if (t != null) t.Join();
+        }
+
+        /// <summary>
+        /// The thread funktion to poll the telemetry data and send TelemetryUpdated events.
+        /// </summary>
+        private void Run()
+        {
+            GTAVAPI lastTelemetryData = new GTAVAPI();
+            Session session = new Session();
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            UdpClient socket = new UdpClient();
+            socket.ExclusiveAddressUse = false;
+            socket.Client.Bind(new IPEndPoint(IPAddress.Any, PORTNUM));
+
+            Log("Listener started (port: " + PORTNUM.ToString() + ") GTAVTelemetryProvider.Thread");
+            while (!isStopped)
+            {
+                try
+                {
+                    // get data from game, 
+                    if (socket.Available == 0)
+                    {
+                        if (sw.ElapsedMilliseconds > 500)
+                        {
+                            IsRunning = false;
+                            IsConnected = false;
+                            Thread.Sleep(1000);
+                        }
+                        continue;
+                    }
+                    else
+                    {
+                        IsConnected = true;
+                    }
+
+                    Byte[] received = socket.Receive(ref _senderIP);
+
+
+                    var alloc = GCHandle.Alloc(received, GCHandleType.Pinned);
+                    GTAVAPI telemetryData = (GTAVAPI)Marshal.PtrToStructure(alloc.AddrOfPinnedObject(), typeof(GTAVAPI));
+
+                    // otherwise we are connected
+                    IsConnected = true;
+
+                    if (telemetryData.PacketId != lastTelemetryData.PacketId)
+                    {
+                        IsRunning = true;
+
+                        sw.Restart();
+
+                        TelemetryEventArgs args = new TelemetryEventArgs(
+                            new GTAVTelemetryInfo(telemetryData, lastTelemetryData));
+                        RaiseEvent(OnTelemetryUpdate, args);
+
+                        lastTelemetryData = telemetryData;
+                    }
+                    else if (sw.ElapsedMilliseconds > 500)
+                    {
+                        IsRunning = false;
+                    }
+
+                    Thread.Sleep(SamplePeriod);
+                }
+                catch (Exception e)
+                {
+                    LogError("GTAVTelemetryProvider Exception while processing data", e);
+                    IsConnected = false;
+                    IsRunning = false;
+                    Thread.Sleep(1000);
+                }
+            }
+
+            socket.Close();
+            IsConnected = false;
+            IsRunning = false;
+        }
+
+    }
+}
